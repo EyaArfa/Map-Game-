b24a5520ace41d4004b2b3f18f301a2c87102f92
Eya Arfa <arfaaya0@gmail.com>
2020-12-04 20:56:13 +0100

    update up to 4.12.2020

---
+++ Assets/Prefabs/CarManipulator.cs
@@ -0,0 +1,203 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Unity.Map;
+using Mapbox.Examples;
+
+
+public class CarManipulator : MonoBehaviour
+{
+    private GameObject newcar, newcam;
+    public GameObject car, cam;
+    public Text currentCar;
+    public GameObject[] cars, cams;
+    public Transform parent;
+    public int i = 0;
+    public AbstractMap map;
+
+
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private float max_distance;
+    [SerializeField]
+    private float maxAngel;
+
+    Vector3 distance_vector;
+
+    [HideInInspector]
+    public bool carInRange = false;
+
+    bool carInStopZone = false;
+
+    [SerializeField]
+    private WaitForSeconds delay;
+
+    [SerializeField]
+    private float stop_radius;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Car").transform;
+        delay = new WaitForSeconds(0.05f);
+
+        StartCoroutine("stop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cars = GameObject.FindGameObjectsWithTag("Car");//to make the script knows  car's gameobject
+        cams = GameObject.FindGameObjectsWithTag("MainCamera");//to make the script knows cam's gameobject
+        currentCar.text = "Car: " + (i + 0).ToString();
+
+        Disable(i);
+
+
+
+        CheckForCar();
+
+        if (carInRange)
+        {
+            if (Vector3.Distance(player.position, transform.position) < stop_radius)
+            {
+                carInStopZone = true;
+                
+            }
+
+        }
+    }
+
+
+    void CheckForCar()
+    {
+        distance_vector = transform.position - player.position;
+        float distance = distance_vector.magnitude;
+
+
+        if (distance < max_distance)
+        {
+
+
+            float angel = Vector3.Angle(this.transform.forward, distance_vector);
+
+            if (angel < maxAngel)
+            {
+                RaycastHit hit;
+                Ray ray = new Ray(transform.position, new Vector3(distance_vector.x, .2f, distance_vector.z));
+
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    Debug.DrawRay(ray.origin, ray.direction);
+                    {
+                        if (hit.transform.gameObject.tag != "Car")
+                        {
+                            carInRange = true;
+
+                        }
+                    }
+
+                }
+
+
+            }
+
+        }
+        else
+        {
+            carInRange = false;
+        }
+
+        if (carInRange)
+        {
+            Stop();
+        }
+
+    }
+
+    void Stop()
+    {
+
+        if (carInStopZone)
+        {
+            Debug.Log("Stop");
+           this.GetComponent<AstronautMouseController>().characterSpeed = 0;
+           // WaitForSeconds(delay) ;
+            this.GetComponent<AstronautMouseController>().characterSpeed = 5;
+            
+
+        }
+
+    }
+
+
+
+
+    public void AddCar()
+    {
+
+        newcar = Instantiate(car, new Vector3(0, 0, 0), Quaternion.identity);
+        newcar.transform.parent = this.transform;// place all added cars in the cars' empty object while adding 
+        newcar.name = "Car" + cars.Length.ToString();
+        newcam = Instantiate(cam, cam.transform.position, cam.transform.rotation);
+        newcam.transform.parent = parent;
+        newcam.name = "Cam" + cams.Length.ToString();
+        newcam.GetComponent<Camera>().enabled = false;
+        newcar.GetComponent<AstronautMouseController>().enabled = false; ;
+        newcar.GetComponent<AstronautMouseController>().map = map;
+        newcam.GetComponent<AstronautMouseController>().cam = newcam.GetComponent<Camera>();
+        newcar.GetComponent<CharacterMovement>().enabled = false;
+        newcar.GetComponent<AstronautDirections>().enabled = false;
+
+    }
+    public void Right()
+    {
+        if (i < cars.Length - 1)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+    public void Left()
+    {
+        if (i > 0)
+        {
+            i--;
+        }
+        else
+        {
+            i = cars.Length - 1;
+        }
+    }
+    public void Disable(int x)
+    {
+        cams[x].GetComponent<Camera>().enabled = true;
+        cars[x].GetComponent<AstronautMouseController>().enabled = true;
+        newcar.GetComponent<CharacterMovement>().enabled = true;
+        newcar.GetComponent<AstronautDirections>().enabled = true;
+        cars[x].GetComponent<AstronautMouseController>().cam = cams[x].GetComponent<Camera>();//to assign cam to the current car 
+
+        for (int j = 0; j < cars.Length; j++)
+        {
+
+            if (x != j)
+            {
+                cams[j].GetComponent<Camera>().enabled = false;
+                cars[j].GetComponent<AstronautMouseController>().enabled = false;
+                cars[j].GetComponent<CharacterMovement>().enabled = false;
+                cars[j].GetComponent<AstronautDirections>().enabled = false;
+
+            }
+        }
+    }
+   
+}
---
+++ Assets/Prefabs/Messaging.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Messaging : MonoBehaviour
+{
+    public CarManipulator script;
+    public GameObject menu;
+    public Dropdown sendTo;
+    public Text msg;
+    public Transform rcv;
+    public GameObject msgCanvas;
+    
+    public void Start()
+    {
+        menu.SetActive(false);
+        script = GameObject.FindWithTag("cars").GetComponent<CarManipulator>();
+    }
+    
+    public void MsgReceive(String m)
+    {
+        msg.text = m;
+        Instantiate(msg, rcv);
+    }
+    public void send(String location)
+    {
+       
+        script.cars[sendTo.value].SendMessage("MsgReceive", "from " + script.cars[script.i].name + ": " + location); 
+
+    }
+    public void ShareLocation()
+    {
+        String location = " location :  " + script.cars[script.i].transform.position.ToString();
+        send(location);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (script.cars[script.i].name == this.name)
+        {
+            msgCanvas.SetActive(true);
+        }
+        else
+        {
+            msgCanvas.SetActive(false);
+        }
+        sendTo.ClearOptions();
+        foreach (GameObject c in script.cars)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = c.name;
+            sendTo.options.Add(option);
+        }
+
+    }
+    
+}
